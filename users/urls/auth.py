7e861ffe480917e91d4cd1cacc17403c3241fbaf
lulu <sarah.j.ting@gmail.com>
2022-12-27 00:16:04 +0900

    fix: fix conflicting app_name

--- users/urls/auth.py
+++ users/urls/auth.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from users.views import RegisterView
 
-app_name = 'users'
+app_name = 'auth'
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
