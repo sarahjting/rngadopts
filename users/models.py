5951992b3cf58684f2aa7750814d7e011cfdf100
lulu <sarah.j.ting@gmail.com>
2022-12-26 23:16:59 +0900

    feat: add model/migration for DiscordUser

--- users/models.py
+++ users/models.py
@@ -9,3 +9,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def is_discord_verified(self):
+        return hasattr(self, 'discord_user')
+
+
+class DiscordUser(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.RESTRICT,
+        related_name='discord_user'
+    )
+    discord_user_id = models.IntegerField(null=True, db_index=True)
+    discord_user_username = models.CharField(max_length=32)
+    discord_user_discriminator = models.CharField(max_length=4)
+    discord_user_avatar = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.discord_user_username}#{self.discord_user_discriminator} ({self.discord_user_id})"
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import DiscordUser, User
+
+
+class UserModelTest(TestCase):
+
+    def test_is_discord_verified_returns_false_when_not_discord_verified(self):
+        user = User()
+        self.assertIs(user.is_discord_verified(), False)
+
+    def test_is_discord_verified_returns_true_when_discord_verified(self):
+        user = User()
+        user.save()
+
+        discord_user = DiscordUser(
+            user=user,
+            discord_user_id=12345678,
+            discord_user_username='foo',
+            discord_user_discriminator='1234',
+            discord_user_avatar='https://discord.com/fakeurl.png'
+        )
+
+        discord_user.save()
+
+        self.assertIs(user.is_discord_verified(), True)
