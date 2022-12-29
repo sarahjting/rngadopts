c4d944bb454c8f70a68ee4eeb50674472e66fdb6
lulu <sarah.j.ting@gmail.com>
2022-12-29 15:45:55 +0900

    dev: add detailed query logging

--- rngadopts/settings.py
+++ rngadopts/settings.py
@@ -202,3 +202,24 @@ if DEBUG:
     CSRF_COOKIE_SAMESITE = None
     SESSION_COOKIE_SAMESITE = None
     CORS_ALLOW_HEADERS = list(default_headers) + ['set-cookie', 'x-xsrf-token']
+    LOGGING = {
+        'version': 1,
+        'filters': {
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            }
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'filters': ['require_debug_true'],
+                'class': 'logging.StreamHandler',
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'level': 'DEBUG',
+                'handlers': ['console'],
+            }
+        }
+    }
