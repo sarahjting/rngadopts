38b786ed00d4b174147b9eb3ee15708b03d56bce
lulu <sarah.j.ting@gmail.com>
2022-12-31 16:49:15 +0900

    feat: add adopts to the admin panel

--- adopts/admin.py
+++ adopts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from adopts.models import Adopt, AdoptMod
 
-# Register your models here.
+
+class AdoptAdmin(admin.ModelAdmin):
+    model = Adopt
+    list_display = ["name", "short_name", "width", "height",]
+
+
+class AdoptModAdmin(admin.ModelAdmin):
+    model = AdoptMod
+    list_display = ["adopt", "mod",]
+
+
+admin.site.register(Adopt, AdoptAdmin)
+admin.site.register(AdoptMod, AdoptModAdmin)
